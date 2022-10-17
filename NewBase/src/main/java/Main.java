c987d48dd587473225b64e0de51e367ef0936b70
Гришин <alexandr_grishin@mail.ru>
2022-10-17 21:08:16 +0500

    my project Start

---
+++ NewBase/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import webSite.SiteConnect;
+
+import java.io.IOException;
+
+public class Main {
+
+    private static final String stringUrl = "http://www.playback.ru/";
+
+    public static void main(String[] args) throws IOException {
+
+        long timeStart = System.currentTimeMillis();
+
+        SiteConnect siteConnect = new SiteConnect();
+//        siteConnect.webConnect(stringUrl);
+//        System.out.println(" = " + siteConnect.getStringList());
+
+        ResultBase resultBase = new ResultBase();
+        resultBase.addResult(stringUrl);
+
+        System.out.println("Timer - " + (System.currentTimeMillis() - timeStart) + " ms.");
+    }
+}
---
+++ NewBase/src/main/java/model/Page.java
@@ -0,0 +1,56 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "search_engine", indexes = @Index(columnList = "path"))
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Path", length = 50, nullable = false)
+    private String path;
+    @Column(name = "Code", nullable = false)
+    private int code;
+    @Column(name = "Content", columnDefinition = "mediumtext", nullable = false)
+    private String content;
+
+
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
+
+//    validate — проверить схему, не вносить изменения в базу данных;
+//    update — обновить схему;
+//    create — создаёт схему, уничтожая предыдущие данные;
+//    create-drop — отказаться от схемы, когда SessionFactory закрывается явно — обычно, когда приложение остановлено.
